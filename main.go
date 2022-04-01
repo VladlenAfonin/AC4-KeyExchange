241a236d2dc851a5ed9e5d9604bb517fb334d69e
Vladlen <vladlenafonin.university@yandex.ru>
2022-04-02 01:29:17 +0300

    Diffie-Hellman with HKDF

--- main.go
+++ main.go
@@ -2,29 +2,54 @@ package main
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
 	"fmt"
+	"io"
 	"log"
 	"math/big"
+
+	"golang.org/x/crypto/hkdf"
 )
 
 const (
-	ErrorRandom = "Unable to generate random number"
-	ErrorPrime  = "Unable to generate prime"
+	ErrorRandom  = "Unable to generate random number"
+	ErrorPrime   = "Unable to generate prime"
+	ErrorReading = "Unable to read from stream"
 )
 
+// Participant model.
 type Participant struct {
+	g *big.Int
+	p *big.Int
+
 	PublicKey  *big.Int
 	secretKey  *big.Int
 	SessionKey *big.Int
 }
 
-func (par *Participant) GenerateSessionKey(publicKey, p *big.Int) {
+// Generates session key from another participant's public key and modulus p.
+func (par *Participant) GenerateSessionKey(publicKey *big.Int) {
+	sessionKey := new(big.Int)
+	sessionKey.Exp(publicKey, par.secretKey, par.p)
+
+	hash := sha256.New
+	hkdf := hkdf.New(hash, sessionKey.Bytes(), nil, nil)
+
 	par.SessionKey = new(big.Int)
-	par.SessionKey.Exp(publicKey, par.secretKey, p)
+	buf := make([]byte, 16)
+
+	_, err := io.ReadFull(hkdf, buf)
+	checkErr(err, ErrorReading)
+
+	par.SessionKey.SetBytes(buf)
 }
 
+// Initializes a new Participant.
 func CreateParticipant(g, p *big.Int) *Participant {
 	par := new(Participant)
+
+	par.p = p
+	par.g = g
 
 	sk, err := rand.Int(rand.Reader, p)
 	checkErr(err, ErrorPrime)
@@ -36,6 +61,7 @@ func CreateParticipant(g, p *big.Int) *Participant {
 	return par
 }
 
+// Checks if err is nil and prints the message extiting the program if true.
 func checkErr(err error, message string) {
 	if err != nil {
 		log.Fatal(message)
@@ -64,8 +90,10 @@ func main() {
 	fmt.Printf("Participant A:\n\tsk = 0x%x\n\tpk = 0x%x\n", parA.secretKey, parA.PublicKey)
 	fmt.Printf("Participant B:\n\tsk = 0x%x\n\tpk = 0x%x\n\n", parB.secretKey, parB.PublicKey)
 
-	parA.GenerateSessionKey(parB.PublicKey, p)
-	parB.GenerateSessionKey(parA.PublicKey, p)
+	// Generate session keys
+
+	parA.GenerateSessionKey(parB.PublicKey)
+	parB.GenerateSessionKey(parA.PublicKey)
 
 	fmt.Printf("Participant A's session key: 0x%v\n", parA.SessionKey)
 	fmt.Printf("Participant B's session key: 0x%v\n", parB.SessionKey)
